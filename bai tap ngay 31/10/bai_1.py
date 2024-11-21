2a6d4450193e20a9bc84a7c4159f18cc56771f9b
bui thi thanh hoa <btthoa.24174600064@sv.uneti.edu.vn>
2024-11-21 21:55:18 +0700

    bai tap ve nha

--- bai tap ngay 31/10/bai_1.py
+++
@@ -1,8 +0,0 @@
-import math
-#Tính năm nhuận
-n = int(input("Nhâp năm cần kiểm tra: "))
-if n > 0:
-    nam_nhuan = n % 4 == 0 and n % 400 == 0
-    print(f"Năm {n} là năm nhuận")
-else:
-    print("Năm không phải là năm nhuận")
